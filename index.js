f73ffd825b17bd144f1382525031bb3df44f8ac0
DOROTHY2322 <kimelidorothy@gmail.com>
2022-11-22 18:18:38 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide =()=>{
+    return 2000/100;
+}
+divide()
+const square = () =>{
+    let x = 2
+    return 4
+}
+square()
+const add = (a,b) => {
+    return a+b
+}
+const addition= (3+4)
+printValue(addition)
